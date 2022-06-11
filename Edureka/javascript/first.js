ba86b818f3c5531a00329d7617f4d3130f6cfbd4
AjayAdsule <ajayadsule5@gmail.com>
2022-06-11 21:41:29 +0530

    i change row

---
+++ Edureka/javascript/first.js
@@ -0,0 +1,19 @@
+let numbers= parseInt ("1010");
+console.log(numbers,(typeof numbers));
+
+// if else statements
+// var a=9
+// if (a>10){
+//     console.log("a is greater than 10")
+// }
+// else{
+//     console.log("a is less than 10")
+// }
+
+var uname= "bhum"
+if (uname="john"){
+    console.log(`hii ${uname} you are super admin`)
+}
+else if (uname="bhum"){
+    console.log(`hii ${uname} you are admin `)
+}
